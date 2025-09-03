c6d24221668d3b9b6022c57336fe5042acb8a53c
Raheela <142294662+raheelam98@users.noreply.github.com>
2025-09-02 22:46:36 -0500

    Python Basic Class

---
+++ Python_Tutorial_RM/class1.py
@@ -0,0 +1,45 @@
+message = "hi"
+print(message)
+
+name = 'hawk '
+if (name):
+    print(name)
+
+name2 = ''
+if (name2):
+    print('with no space',name2)  
+
+name = ' '
+if (name):
+    print('with space', name)       
+
+m=0;
+if m:
+    print("value isn't save with 0 (zero : False)")  
+
+n=1;
+if n:
+    print('value is save with 1 (any-value : True)')      
+
+# float
+# ceil
+
+# https://docs.python.org/3/tutorial/introduction.html
+
+num1 = 4 * 3.75 - 1
+print(num1)  
+
+
+### ===================
+
+# 8400
+# _+3000 #underscore helps to use the last value so that new variables
+#             #are not created cause they consume memory. secondly, we dont
+#             #want to update original value
+# 11400
+# _-3500.56
+# 7899.4400000000005
+# round(_,2)
+# 7899.44
+# left
+# 9900
---
+++ Python_Tutorial_RM/class2.py
@@ -0,0 +1,42 @@
+a = 9000
+deduction=500
+new = a - deduction
+print(new)  ## output :- 8500
+
+print(a) ## output :- 9000
+print(''' 
+hi / 2 hi
+      \ 3hi   
+
+ ''')
+
+f = '3' '+4'
+print(f)
+
+l = 3 + 4
+print(l)
+
+k = 'Py' 'thon'
+print(k)
+
+## ========== ##
+
+name = ['Jhone']
+print(len(name))
+
+name1 = ['JhoneSmallBrother']
+# name1[1:3]
+print("hi",name1[-1:3])
+
+my_list :(int) = [8,9]
+my_list.append(1)
+my_list.append(2)
+print(my_list) 
+my_list.append('abc')
+print(my_list)
+
+f= [1,2.,3]
+f[]
+
+
+
